e7bef2f891a0831c93b4b33f3b68b2aa2cd649d8
Klyuchnikov Dmitry <96Dimian@inbox.ru>
2022-09-06 16:36:42 +0300

    create about-us & why-are-we blocks

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@ menuButton.addEventListener('click', function () {
   navBlock.classList.toggle('scale');
   menuButton.classList.toggle('active');
 });
+
+//--скрывать хедер--
+let lastScroll = 0;
+const defaultOffset = document.documentElement.clientHeight;
+const header = document.querySelector('.header');
+
+const scrollPosition = () => window.pageYOffset;
+const isContainHide = () => header.classList.contains('_hide');
+
+window.addEventListener('scroll', function() {
+  if (scrollPosition() > lastScroll && !isContainHide() && scrollPosition() > defaultOffset) {
+    //scroll down
+    header.classList.add('_hide');
+  } else if (scrollPosition() < lastScroll && isContainHide()) {
+    //scroll up
+    header.classList.remove('_hide');
+  }
+
+  lastScroll = scrollPosition();
+});
