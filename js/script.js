55b3a4ef262fb4aabd96694c91c4d7f214d8f2a5
Klyuchnikov Dmitry <96Dimian@inbox.ru>
2022-10-12 16:47:05 +0300

    feat: рекаллс блок добавить

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ menuButton.addEventListener('click', function () {
 
 //--скрывать хедер--
 let lastScroll = 0;
-const defaultOffset = document.documentElement.clientHeight;
+const defaultOffset = document.documentElement.clientHeight + 100;
 const header = document.querySelector('.header');
 
 const scrollPosition = () => window.pageYOffset;
@@ -37,5 +37,32 @@ sliderHeading.forEach((el) => {
     document.querySelectorAll('.slider__paragraph').forEach((el) => el.classList.remove('slider__paragraph_visible'));
 
     sliderParagraph.classList.toggle('slider__paragraph_visible');
+  });
+});
+
+// выплывание карточек отзывов
+const recallsLeftCardsArray = Array.from(document.querySelectorAll('.card_left'));
+const recallsRightCardsArray = Array.from(document.querySelectorAll('.card_right'));
+
+document.addEventListener('scroll', function setVisibilityOnCard() {
+  const lastScroll = window.pageYOffset + document.documentElement.clientHeight;
+
+  recallsLeftCardsArray.forEach((el) => {
+    const distanceFromCardToTop = el.getBoundingClientRect().top + window.pageYOffset;
+
+    if (lastScroll >= distanceFromCardToTop + 100) {
+      el.classList.add('card_type_visible_left');
+    } else if (lastScroll < distanceFromCardToTop) {
+      el.classList.remove('card_type_visible_left');
+    };
+  });
+  recallsRightCardsArray.forEach((el) => {
+    const distanceFromCardToTop = el.getBoundingClientRect().top + window.pageYOffset;
+
+    if (lastScroll >= distanceFromCardToTop + 100) {
+      el.classList.add('card_type_visible_right');
+    } else if (lastScroll < distanceFromCardToTop) {
+      el.classList.remove('card_type_visible_right');
+    };
   });
 });
